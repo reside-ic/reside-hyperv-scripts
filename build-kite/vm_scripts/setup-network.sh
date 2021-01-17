cadb53be5fa9ea5cea5de7de0ec0baf76e2e8220
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-01-17 19:53:15 +0000

    New setup-network

--- build-kite/vm_scripts/setup-network.sh
+++ build-kite/vm_scripts/setup-network.sh
@@ -1,3 +1,15 @@
-sudo ip addr add $IP/24 dev eth0
-sudo ip route add default via $GW dev eth0
-sudo ip link set eth0 up
+sudo cat <<EOF > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: false
+      dhcp6: false
+      addresses: [$IP/24]
+      gateway4: $GW
+      nameservers:
+        addresses: [8.8.8.8,8.8.4.4]
+EOF
+
+sudo netplan apply

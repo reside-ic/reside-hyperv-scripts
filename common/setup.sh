88355299e68e200c3875bdf5ade28bf9e05352af
Alex <alex.hill@gmail.com>
2023-02-07 14:39:48 +0000

    add local bin path to PATH

--- common/setup.sh
+++ common/setup.sh
@@ -44,3 +44,5 @@ chmod 755 /etc/cron.daily/check_reboot_required
 
 sed -i 's|#PasswordAuthentication yes|PasswordAuthentication no|' /etc/ssh/sshd_config
 sed -i 's|#PubkeyAuthentication yes|PubkeyAuthentication yes|' /etc/ssh/sshd_config
+
+echo 'export PATH="$HOME/.local/bin:$PATH"' > /etc/profile.d/add-local-bin.sh

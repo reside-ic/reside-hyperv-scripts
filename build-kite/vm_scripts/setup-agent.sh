c5b5d6ac42f6a200a0e1004024f0c4f7fd146d78
Robert Ashton <r.ashton@imperial.ac.uk>
2023-10-25 16:33:37 +0100

    Update agent setup script to use latest pgp key

--- build-kite/vm_scripts/setup-agent.sh
+++ build-kite/vm_scripts/setup-agent.sh
@@ -2,8 +2,8 @@
 set -e
 
 ## Setup as an agent
-echo "deb https://apt.buildkite.com/buildkite-agent stable main" | sudo tee /etc/apt/sources.list.d/buildkite-agent.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 32A37959C2FA5C3C99EFBC32A79206696452D198
+curl -fsSL https://keys.openpgp.org/vks/v1/by-fingerprint/32A37959C2FA5C3C99EFBC32A79206696452D198 | sudo gpg --dearmor -o /usr/share/keyrings/buildkite-agent-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/buildkite-agent-archive-keyring.gpg] https://apt.buildkite.com/buildkite-agent stable main" | sudo tee /etc/apt/sources.list.d/buildkite-agent.list
 sudo apt-get update && sudo apt-get install -y buildkite-agent
 
 # Enable passwordless login

44da1cf2b132f5e386586a01fe48db6defb82a51
Rob Ashton <r.ashton@imperial.ac.uk>
2023-12-14 15:04:51 +0000

    Add note about port to start script

--- common/start-prometheus-node-exporter.sh
+++ common/start-prometheus-node-exporter.sh
@@ -4,4 +4,5 @@ set -ex
 wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
 tar xvfz node_exporter-*.*-amd64.tar.gz
 cd node_exporter-*.*-amd64
+# Exposes metrics on port 9100 by default
 nohup ./node_exporter > node_exporter.log 2>&1 &

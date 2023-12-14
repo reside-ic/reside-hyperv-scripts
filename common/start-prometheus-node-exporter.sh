b803fe9d29eae149c14a78b09c4a3bdc26a0c953
Rob Ashton <r.ashton@imperial.ac.uk>
2023-12-14 17:45:19 +0000

    Use machine metrics to start node exporter

--- common/start-prometheus-node-exporter.sh
+++ common/start-prometheus-node-exporter.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
-tar xvfz node_exporter-*.*-amd64.tar.gz
-cd node_exporter-*.*-amd64
-# Exposes metrics on port 9100 by default
-nohup ./node_exporter > node_exporter.log 2>&1 &
+git clone https://github.com/vimc/machine-metrics.git
+cd machine-metrics
+./install
